e5287a83723bc40fdffc7478bae2366db4257558
_alisa.na <alisanuralistia10@gmail.com>
2025-05-17 13:31:29 +0700

    Migration

---
+++ 2025_05_11_043821_create_cars_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('tipe');
+            $table->year('tahun');
+            $table->decimal('harga_per_hari', 10, 2);
+            $table->string('status')->default('tersedia');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+};
